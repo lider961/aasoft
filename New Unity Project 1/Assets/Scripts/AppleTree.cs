7d5ba992c5eb79f21665f59868474fc142ed86c0
unknown <oyuye001@MDMA325W14.ksamk.ad>
2015-07-17 12:28:36 +0300

    no message

---
+++ New Unity Project 1/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+	// Prefab for creating apples
+	
+	public GameObject applePrefab;
+	public GameObject applePrefab2;
+	public GameObject applePrefab3;
+		
+	// Rate at which the apples are created
+	public float secondsBetweenAppleDrops = 1f;
+	
+	public float speed = 2f;
+	// Distance where the AppleTree turns around
+	public float leftAndRightEdge = 6f;
+	// Chance that the tree will change direction
+	public float chanceToChangeDirection = 0.1f;
+	
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("DropApple", 1f, secondsBetweenAppleDrops);
+	}
+	void DropApple()
+	{
+		int randomNumber = Random.Range (1, 4);
+		if (randomNumber == 1) {
+			GameObject apple = Instantiate (applePrefab) as GameObject;
+			apple.transform.position = transform.position;
+		}
+		else if(randomNumber==2){
+			GameObject apple = Instantiate (applePrefab2) as GameObject;
+			apple.transform.position = transform.position;
+		}
+		else{
+			GameObject apple = Instantiate (applePrefab3) as GameObject;
+			apple.transform.position = transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 savedTreePosition = transform.position; // a
+		savedTreePosition.x += speed * Time.deltaTime; // b
+		transform.position = savedTreePosition; 
+		if (savedTreePosition.x < -leftAndRightEdge) { // left edge
+			speed = Mathf.Abs (speed); // Move right
+		}
+		else if (savedTreePosition.x > leftAndRightEdge) { // right edge
+			speed = -Mathf.Abs(speed); // move left
+		}
+	}
+	void FixedUpdate() {
+		// changing direction randomly
+		if (Random.value < chanceToChangeDirection) {
+			speed *= -1; //change direction
+		}
+	}
+}
---
+++ New Unity Project 1/Assets/Scripts/Basket.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour {
+	public float speed = 0.1f;
+	public Text scoreText;
+	public Text scoreText2;
+	public Text scoreText3;
+	void Start () {
+		scoreText.text = "0";
+	}
+	void Update () {
+		/*
+		Vector3 mouse2Dpos = Input.mousePosition;
+		Vector3 mouse3Dpos = Camera.main.ScreenToWorldPoint (mouse2Dpos);
+		Vector3 pos = this.transform.position;
+		pos.x = mouse3Dpos.x;
+		this.transform.position = pos;*/
+
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x--;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x++;
+			this.transform.position = position;
+		}
+
+
+	}
+	void OnTriggerEnter(Collider collider)
+	{
+		// Find out what hit the basket
+		GameObject collidedWith = collider.gameObject;
+		// if it's an apple destroy it
+		if (collidedWith.CompareTag ("apple")) {
+			Destroy(collidedWith);
+			int score = int.Parse (scoreText.text);
+			score ++;
+			scoreText.text = score.ToString ();
+		}
+		else if (collidedWith.CompareTag ("apple_g")) {
+			Destroy(collidedWith);
+			int score = int.Parse (scoreText2.text);
+			score ++;
+			scoreText2.text = score.ToString ();
+		}
+		else if (collidedWith.CompareTag ("apple_y")) {
+			Destroy(collidedWith);
+			int score = int.Parse (scoreText3.text);
+			score ++;
+			scoreText3.text = score.ToString ();
+		}
+	}
+}
